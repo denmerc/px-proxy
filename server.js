56f155c7d5a90c1317fefff2f1497682b7b1f733
Dennis Mercado <dmac@Denniss-MacBook-Pro.local>
2016-05-13 18:07:12 -0700

    register test proxy

--- server.js
+++ server.js
@@ -1,20 +1,4 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-var primes = require('./primes.js');
+var proxy = require('redbird')({port:3000});
 
-app.get('/', function(req, res){
-    res.status(200).send('Testing and Debugging Sample');
-})
+proxy.register("google.com", "http://www.yahoo.com");
 
-app.get('/isPrime/:number', function(req, res){
-    res.status(200).send(primes.isPrime(req.params.number));    
-})
-
-var server = app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
-});
-
-exports.stop = function(){
-    server.close();
-}
